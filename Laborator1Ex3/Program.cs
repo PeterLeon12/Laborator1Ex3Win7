5b7b07f8352012c446270177889d391f9e314c6d
user <user@DESKTOP-FN026Q4>
2024-04-06 15:03:32 +0300

    Add project files.

---
+++ Laborator1Ex3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Ex3
+//Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la
+//tastaura
+
+
+
+using System.Numerics;
+
+Console.WriteLine("Scrieti un numar:");
+BigInteger numar = BigInteger.Parse(Console.ReadLine());
+
+BigInteger ultimulnumar = numar % (10);  
+Console.WriteLine("Ultima cifra din numarul" + " " + numar  + " " + "este" + " " + ultimulnumar);
